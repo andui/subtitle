53858fc44fff7137f71704a9b559c5858576d79e
Ying <yzhengyy@gmail.com>
2016-01-03 00:39:26 +0800

    init commit

---
+++ hello.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+import subtitle
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'tmp/'
+ALLOWED_EXTENSIONS = ['srt', 'txt']
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename, index):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] == ALLOWED_EXTENSIONS[index]
+
+def ext(filename):
+	if '.' in filename:
+		return filename.rsplit('.', 1)[1]
+	else:
+		return ""
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+	if request.method == 'POST':
+		files = []
+		files.append(request.files['srt'])
+		files.append(request.files['cut'])
+		for i in range(len(files)):
+			file = files[i]
+			if file and allowed_file(file.filename, i):
+				print file.filename
+				filename = secure_filename(file.filename)
+				file.save(filename)
+				if ext(filename)=='srt':
+					srtFile = filename
+				else:
+					cutFile = filename  
+        dest = subtitle.main(srtFile,cutFile)
+        return redirect(url_for('uploaded_file', filename=dest))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ subtitle.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+import re
+import pysrt
+
+# parse time string to datetime object with given format, eg. timeStr =
+# '16:31:32.123'
+def str2Dt(timeStr):
+    return datetime.strptime(timeStr, '%H:%M:%S,%f')
+
+# parse datetime object to time string with given format
+
+
+def dt2Str(dateTime):
+    return dateTime.strftime('%H:%M:%S,%f')[:-3]
+
+
+def shift(part, delta, factor):
+    part.shift(hours=delta.hour * factor)
+    part.shift(minutes=delta.minute * factor)
+    part.shift(seconds=delta.second * factor)
+    part.shift(milliseconds=delta.microsecond / 1000 * factor)
+
+def main(srtFile, cutFile):
+    dest = srtFile[:-4] + "_cut.srt"
+    # read from the cut file to get a list of [start, end]
+    start = []
+    end = []
+
+    lines = [line.rstrip('\n') for line in open(cutFile)]
+    pattern = re.compile("^(\d+\.)")
+    lineNum = 0
+    for line in lines:
+        lineNum += 1
+        strs = line.split()
+        # get the diff
+        if lineNum == 2:
+            factor = 1 if strs[1] == '+' else -1
+            diff = str2Dt(strs[2])
+        elif pattern.match(line):
+            # need to adjust this with the offset?
+            start.append(str2Dt(strs[1]))
+            end.append(str2Dt(strs[2]))
+
+    # now read the srt file and out put the cutSrt
+    subs = pysrt.open(srtFile)
+    res = subs
+
+    for i in range(len(start)):
+        # print i
+        part = subs.slice(
+            starts_after={'hours': start[i].hour, 'minutes': start[i].minute, 'seconds': start[
+                i].second, 'milliseconds': start[i].microsecond / 1000},
+            ends_before={'hours': end[i].hour, 'minutes': end[i].minute, 'seconds': end[i].second, 'milliseconds': end[i].microsecond / 1000})
+
+        if i == 0:
+            delta = start[i]
+            shift(part, delta, -1)
+            res = part
+        else:
+            delta += start[i] - end[i - 1]
+            shift(part, delta, -1)
+            res.extend(part)
+
+    # adjust according to the diff between original video and srt
+    shift(res, diff, factor)
+
+    # adjest according to length of piantou
+
+    res.save(dest, encoding='utf-8')
+    return dest
